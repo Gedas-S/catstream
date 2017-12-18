8b78b45e43f975e2040e2b5fad54149a05ffcdc6
Gedas <g.simaitis@gmail.com>
2017-12-18 03:41:02 +0200

    a few comments

--- catstream/catstream.py
+++ catstream/catstream.py
@@ -28,29 +28,36 @@ def receive_cat():
     """take a post request with an image file, show the uploaded image and say if it's a cat"""
     image = request.files['image']
 
+    # check if a file was received
     if not image:
         return render_template('error.html', message="I lost the cat, did you send?")
+    # check if received file is an image
     if image.filename.split('.')[-1] not in ALLOWED_EXTENSIONS:
         return render_template('error.html', message=(
             "Sorry, I meant picture of cat, not actual cat. Does not look like a picture to me. I like %s and %s."
             % (', '.join(ALLOWED_EXTENSIONS[:-1]), ALLOWED_EXTENSIONS[-1])))
+    
+    # try to open the image
     try:
         img = Image.open(image)
-        category_num = predict_category(PREPROCESSING_TRANSFORM(img), cat_net)
     except OSError:
         return render_template('error.html', message=
                                "I does not understands your cat. Is your cat corrupted?")
-    except RuntimeError:
+    # try to run the image through the neural net
+    try:
+        category_num = predict_category(PREPROCESSING_TRANSFORM(img), cat_net)
+    except RuntimeError: # this usually happens when the convolution layers run out of bounds
         return render_template('error.html', message=
                                "I cannot make out anything, could you send a bigger photo?")
 
+    # create message depending on whether we think it's a cat
     if is_cat(category_num):
         response_string = "Cat!!! (%s)" % CLASSES[category_num]
     else:
         response_string = ("I don't think it's a cat, looks like a %s to me..."
                            % CLASSES[category_num])
 
-    image.seek(0)
+    image.seek(0) # reset the cursor on the FileStorage object
     context = {
         'response_string': response_string,
         'image_type': image.headers['Content-Type'],
@@ -58,8 +65,9 @@ def receive_cat():
         }
     return render_template('cat.html', **context)
 
-print("Loading cnn, please stand by.")
+print("Loading CNN, please stand by.")
 cat_net = get_network() # pylint: disable=invalid-name
+print("CNN loaded, proceeding.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
