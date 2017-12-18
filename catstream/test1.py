e1c6a2841d72f38bfb18b8451673754dd39608d8
Gedas <g.simaitis@gmail.com>
2017-12-18 03:26:45 +0200

    move test images into a folder

--- catstream/test1.py
+++ catstream/test1.py
@@ -13,7 +13,7 @@ class TestStandalone(unittest.TestCase):
     def test_b64_image_encoding(self):
         from base64 import standard_b64encode
         from werkzeug.datastructures import FileStorage
-        rdata = FileStorage(open('dot.png', 'rb')) # taken from wiki, public domain
+        rdata = FileStorage(open('test_files/dot.png', 'rb')) # taken from wiki, public domain
         bimg = standard_b64encode(rdata.read())
         self.assertEqual(bimg, b'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==')
 
@@ -24,7 +24,7 @@ class TestResnetNetwork(unittest.TestCase):
 
     def test_resnet_image(self):
         # I just drew the image and hereby release it into the public domain :P
-        img = Image.open('test_image.jpg')
+        img = Image.open('test_files/test_image.jpg')
         img = model_resnet.PREPROCESSING_TRANSFORM(img)
         cat = utils.category(img, self.net)
         self.assertEqual(type(cat), int)
