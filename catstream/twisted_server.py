09c06178f098b6d1b9b207cfa4b8230bea2d0342
Gedas <g.simaitis@gmail.com>
2017-12-18 07:24:54 +0200

    ssl!

--- catstream/twisted_server.py
+++ catstream/twisted_server.py
@@ -2,11 +2,29 @@
 # pylint: disable=invalid-name
 # pylint: disable=no-member
 from twisted.internet import reactor
+from twisted.web.resource import Resource
 from twisted.web.server import Site
+from twisted.web.util import redirectTo
 from twisted.web.wsgi import WSGIResource
+import pem
 from catstream import app
 
 catstream_site = WSGIResource(reactor, reactor.getThreadPool(), app)
 
-reactor.listenTCP(80, Site(catstream_site))
+ctxFactory = pem.twisted.certificateOptionsFromFiles(
+    'key.pem', 'cert_and_chain.pem')
+# thanks Let's Encrypt and ZeroSSL!
+reactor.listenSSL(443, Site(catstream_site), ctxFactory)
+
+# redirect http traffic to https
+class HttpsRedirector(Resource):
+    """redirect traffic to https"""
+    def render(self, request):
+        url_path = request.URLPath()
+        url_path.scheme = 'https'
+        return redirectTo(bytes(str(url_path), 'utf-8'), request)
+    def getChild(self, path, request):
+        return self
+reactor.listenTCP(80, Site(HttpsRedirector()))
+
 reactor.run()
